830e21be2d48d40c285922c01a6323ca40d20085
tlessman <tlessman@uoregon.edu>
2017-01-12 08:07:18 +0000

    corrected missing quotation in installer, added daily report and question to notes.

--- install_daemons.sh
+++ install_daemons.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo '||| OSNAP Daemon Tool |||';
-echo This tool will clone Postgres 2.9.x, then build and install Postgres.';
+echo 'This tool will clone Postgres 2.9.x, then build and install Postgres.';
 echo 'It will also download, build and install Apache httpd 2.4.25 with mod_wsgi.';
 echo 'Cloning Postgres from GitHub...';
 git clone https://github.com/postgres/postgres.git;
