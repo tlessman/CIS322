77f9d2b2651201d89e69e403b908e5029a06e3f5
Theodore Lessman <tlessman@uoregon.edu>
2017-01-13 02:33:06 +0000

    install_daemons now downloads postgres and apache. add cls.sh to quickly erase data downloaded for troubleshooting purposes.

---
+++ clr.sh
@@ -0,0 +1,4 @@
+echo 'THIS WILL REMOVE DATA IF PRESENT'
+rm -rf postgres
+rm -rf httpd-2.4.25.tar.bz2
+echo 'REMOVED'
--- install_daemons.sh
+++ install_daemons.sh
@@ -1,4 +1,13 @@
-echo "hello";
-echo "world";
-ls -l /home;
-ls -l /$HOME;
+echo " "
+echo '||| OSNAP Daemon Tool |||'
+echo " "
+# this tool will download, configure, and install Postgres 2.9.x and Apache httpd 2.4.25
+echo 'Tool is downloading assets...'
+echo 'DOWNLOADING Postgres'
+git clone https://github.com/postgres/postgres.git
+echo 'DONE'
+echo 'DOWNLOADING Apache httpd'
+curl -# -O https://www-eu.apache.org/dist//httpd/httpd-2.4.25.tar.bz2
+echo 'DONE'
+ls -l
+
