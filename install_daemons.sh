b2ad975dbba261d0764e5d4eaca6245be66010c5
Theodore Lessman <tlessman@uoregon.edu>
2017-01-14 04:15:08 +0000

    on install_daemons.sh, rearranged blocks and updated to checkout branch 9.5.x

--- install_daemons.sh
+++ install_daemons.sh
@@ -2,19 +2,14 @@ echo $1
 echo " "
 echo '||| OSNAP Daemon Tool |||'
 echo " "
+
 # this tool will download, configure, and install Postgres 2.9.x and Apache httpd 2.4.25
+
 echo 'Tool is downloading assets...'
 echo 'DOWNLOADING Postgres'
 git clone https://github.com/postgres/postgres.git
+git checkout REL9_5_STABLE
 echo 'DONE'
-echo 'DOWNLOADING Apache httpd'
-curl -# -O https://www-eu.apache.org/dist//httpd/httpd-2.4.25.tar.bz2
-echo 'UNPACKING...'
-tar -xjf httpd-2.4.25.tar.bz2
-echo 'DONE'
-echo ' '
-echo ' '
-echo ' '
 
 echo 'Tool will configure and install PostgreSQL...'
 echo 'CONFIGURING...'
@@ -27,12 +22,19 @@ make install
 cd ..
 echo 'DONE'
 echo ' '
-echo ' '
+
+
+
+echo 'DOWNLOADING Apache httpd'
+curl -# -O https://www-eu.apache.org/dist//httpd/httpd-2.4.25.tar.bz2
+echo 'UNPACKING...'
+tar -xjf httpd-2.4.25.tar.bz2
+echo 'DONE'
 echo ' '
 
-cd httpd-2.4.25
 echo 'Tool will configure and install Apache...'
 echo 'CONFIGURING...'
+cd httpd-2.4.25
 ./configure --prefix=$1
 echo 'Preparing...'
 make
