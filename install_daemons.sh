95097ed1596e3bdf274fcf323046770d19936726
Theodore Lessman <tlessman@uoregon.edu>
2017-01-16 02:09:55 +0000

    cleaned up install script

--- install_daemons.sh
+++ install_daemons.sh
@@ -5,70 +5,62 @@ echo " "
 
 # this tool will download, configure, and install Postgres 2.9.x and Apache httpd 2.4.25
 
+echo 'Tool will download, configure and install PostgreSQL 9.5.x and Apache httpd 2.4.25'
 
-echo 'DOWNLOADING Postgres'
+echo 'Downloading Postgres'
 git clone https://github.com/postgres/postgres.git
-echo ' '
-
-#git branch -r
-echo ' '
-echo ' '
-
-#git checkout -b REL9_5_STABLE origin/REL9_5_STABLE
-echo ' '
-echo ' '
-
 
 echo 'DONE'
 
-echo 'Tool will configure and install PostgreSQL...'
-echo 'CONFIGURING...'
+echo 'Configuring Postgres'
 cd postgres
 ./configure --prefix=$1
-echo 'Preparing...'
+
+echo 'Preparing Postgres'
 make
-echo 'Installing'
+
+echo 'Installing Postgres'
 make install
-echo ' '
-echo ' '
+
+echo 'Switching to 9.5.x'
 
 git branch -r
-echo ' '
-echo ' '
 
 git checkout -b REL9_5_STABLE origin/REL9_5_STABLE
-echo ' '
 
-echo 'CONFIGURING...'
+echo 'Configuring 9.5.x'
 cd postgres
 ./configure --prefix=$1
-echo 'Preparing...'
+
+echo 'Preparing 9.5.x'
 make
-echo 'Installing'
+
+echo 'Installing 9.5.x'
 make install
-echo ' '
-echo ' '
-echo ' '
+
 cd ..
 echo 'DONE'
-echo ' '
 
 
 
-echo 'DOWNLOADING Apache httpd'
+echo 'Downloading Apache httpd'
 curl -# -O https://www-eu.apache.org/dist//httpd/httpd-2.4.25.tar.bz2
-echo 'UNPACKING...'
-tar -xjf httpd-2.4.25.tar.bz2
-echo 'DONE'
-echo ' '
 
-echo 'Tool will configure and install Apache...'
-echo 'CONFIGURING...'
+echo 'Unpacking Apache...'
+tar -xjf httpd-2.4.25.tar.bz2
+
+echo 'DONE'
+
+echo 'Configuring Apache...'
 cd httpd-2.4.25
 ./configure --prefix=$1
-echo 'Preparing...'
-make
-echo 'Installing'
-make install
-echo 'DONE'
 
+echo 'Preparing Apache...'
+make
+
+echo 'Installing Apache'
+make install
+
+echo 'DONE'
+echo ' '
+echo '||| OSNAP Daemon Tool Complete |||'
