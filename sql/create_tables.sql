5806efc75e647aa018c26e57e00c44acdc4d34d7
Theodore <tlessman@uoregon.edu>
2017-01-23 22:24:34 +0000

    created tables in create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,11 +1,22 @@
-create table assets ( asset_pk serial, product_fk int, asset_tag varchar(10), description varchar(50), alt_description(50));
-
-create table products ( product_pk serial, vendor varchar(10), description varchar(50), alt_description(50));
-
+/*Asset Tables*/
+create table assets ( asset_pk serial, product_fk int, asset_tag varchar(16), description varchar(64), alt_description(64));
+create table products ( product_pk serial, vendor varchar(16, description varchar(64), alt_description(64));
 create table vehicles ( vehicle_pk serial, asset_fk int);
-
-create table facilities ( facility_pk serial, fcode varchar(6), common_name varchar(25), location varchar(100));
-
+create table facilities ( facility_pk serial, fcode varchar(8), common_name varchar(32), location varchar(100));
 create table asset_at ( asset_fk int, facility_fk int, arrive_dt timestamp, depart_dt timestamp);
+create table convoys ( convoy_pk serial, request varchar(16), source_fk int, dest_fk int, depart_dt timestamp, arrive_dt timestamp);
+create table used_by ( vehicle_fk int, convoy_fk int);
+create table asset_on ( asset_fk int, convoy_fk int, load_dt timestamp, unload_dt timestamp);
+
+/*User Tables*/
+create table users ( user_pk serial, username varchar(16), active boolean);
+create table roles ( role_pk serial, title varchar(16));
+create table user_is ( user_fk int, role_fk int);
+create table user_supports ( user_ft int, facility_fk int);
+
+/*Security Tables*/
+create table levels (level_pk serial, abbrv varchar(8), comment varchar(32));
+create table compartments (compartment_pk serial, abbrv varchar(8), comment varchar(32));
+create table security_tags (tag_pk serial, level_fk int, compartment_fk int, user_fk int, product_fk int, asset_fk int);
 
 
