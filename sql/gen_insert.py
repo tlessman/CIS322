e99fe8c96a5e306be953f0847a14850d2a406a10
Theodore <tlessman@uoregon.edu>
2017-01-24 01:46:19 +0000

    practicing with sql commands

--- sql/gen_insert.py
+++ sql/gen_insert.py
@@ -13,12 +13,13 @@ conn = psycopg2.connect( dbname = 'lost', host = '127.0.0.1', port = 5432)
 cur = conn.cursor()
 
 #access file
+csv_data = csv.reader("osnap_legacy/.security_compartments.csv")
 
-with open ("osnap_legacy/convoys.csv", "r") as f:
-    reader = csv.reader(f)
-    columns = next(reader)
-    query = 'insert into convoys({0}) values ({1})'
-    query = query.format(','.join(columns), ','.join('?' * len(columns)))
-    for data in reader:
-        cursor.execute(query, data)
-    cursor.commit()
+rows = csv_data.split('\n')
+first_row = next(csv_data)
+for row in rows:
+    cur.execute(insert into compartments (abbrv, comment);)
+
+cur.commit()
+
+
