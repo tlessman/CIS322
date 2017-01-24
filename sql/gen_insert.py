78f315e7464874b8827ba80c2b6271ac94c89ce5
Theodore <tlessman@uoregon.edu>
2017-01-24 00:20:30 +0000

    added trail read of convoys to .py

--- sql/gen_insert.py
+++ sql/gen_insert.py
@@ -1,12 +1,24 @@
 #gen_insert // python script using psychopg to execute sql commands to read, clean and insert data from csv to lost db
 
-dbname 5432
+#dbname 5432
 
 import psycopg2
 import csv
 import sys
 
-ifile open("osnap_legacy/convoys.csv", "rb")
-reader = csv.reader(ifile)
 
+#database connection setup
+#conn = psycopg2.connect(dbname=sys.argv[1], host = '127.0.0.1', port = int(sys.argv[2]))
+conn = psycopg2.connect( dbname = 'lost', host = '127.0.0.1', port = 5432)
+cur = conn.cursor()
 
+#access file
+
+with open ("osnap_legacy/convoys.csv", "r") as f:
+    reader = csv.reader(f)
+    columns = next(reader)
+    query = 'insert into convoys({0}) values ({1})'
+    query = query.format(','.join(columns), ','.join('?' * len(columns)))
+    for data in reader:
+        cursor.execute(query, data)
+    cursor.commit()
