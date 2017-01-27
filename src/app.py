f0974c56673f1075a43af7556cd4cfabd46b2f1b
Theodore <tlessman@uoregon.edu>
2017-01-27 20:00:09 +0000

    initial commitwith framework started for flask app

---
+++ src/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/filter')
+def filter():
+    return render_template('filter.html')
+
+@app.route('/inventory')
+def inventory():
+    return render_template('inventory.html')
+
+@app.route('/transit')
+def transit():
+    return render_template('transit.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('logout.html')
+
