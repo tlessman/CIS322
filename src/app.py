085e1fcd6e08428fd84af588614e679bf546c8c4
Theodore <tlessman@uoregon.edu>
2017-01-28 03:15:01 +0000

    set up flask to run from src and to bypass wsgi

--- src/app.py
+++ src/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+"""
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/login')
+"""
+@app.route('/')
 def login():
     return render_template('login.html')
 
