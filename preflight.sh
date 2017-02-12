7aac96e8deb2ee1aedafc4d6d1786884a36d9ecc
Theodore <tlessman@uoregon.edu>
2017-02-12 00:03:57 +0000

    copied list_products from dellsworth, encountered error with sec_compartment not existing in db

--- preflight.sh
+++ preflight.sh
@@ -1,4 +1,6 @@
-
+#!/bin/bash
+initdb lost
+pg_ctl -D lost -l logfile start
 createdb lost
 psql lost -f ./sql/create_tables.sql
 #python3 ./sql/gen_insert.py
--- src/app.py
+++ src/app.py
@@ -62,6 +62,101 @@ def suspend_user():
     #req['timestamp]'
     #req['username']
 
+@app.route('/rest/list_products', methods=('POST',))
+def list_products():
+    """This function is huge... much of it should be broken out into other supporting
+        functions"""
+    
+    # Check maybe process as plaintext
+    if request.method=='POST' and 'arguments' in request.form:
+        req=json.loads(request.form['arguments'])
+    # Unmatched, take the user somewhere else
+    else:
+        redirect('rest')
+    
+    # Setup a connection to the database
+    conn = psycopg2.connect(dbname=dbname,host=dbhost,port=dbport)
+    cur  = conn.cursor()
+    
+    # If execution gets here we have request json to work with
+    # Do I need to handle compartments in this query?
+    if len(req['compartments'])==0:
+        print("have not compartment")
+        # Just handle vendor and description
+        SQLstart = """select vendor,description,string_agg(c.abbrv||':'||l.abbrv,',')
+from products p
+left join security_tags t on p.product_pk=t.product_fk
+left join sec_compartments c on t.compartment_fk=c.compartment_pk
+left join sec_levels l on t.level_fk=l.level_pk"""
+        if req['vendor']=='' and req['description']=='':
+            # No filters, add the group by and query is ready to go
+            SQLstart += " group by vendor,description"
+            cur.execute(SQLstart)
+        else:
+            if not req['vendor']=='' and not req['description']=='':
+                req['vendor']="%"+req['vendor']+"%"
+                req['description']="%"+req['description']+"%"
+                SQLstart += " where description ilike %s and vendor ilike %s group by vendor,description"
+                cur.execute(SQLstart,(req['description'],req['vendor']))
+            elif req['vendor']=='':
+                req['description']="%"+req['description']+"%"
+                SQLstart += " where description ilike %s group by vendor,description"
+                cur.execute(SQLstart,(req['description'],))
+            elif req['description']=='':
+                req['vendor']="%"+req['vendor']+"%"
+                SQLstart += " where vendor ilike %s group by vendor,description"
+                cur.execute(SQLstart,(req['vendor'],))
+    else:
+        print("have compartment %s"%len(req['compartments']))
+        # Need to handle compartments too
+        SQLstart = """select vendor,description,string_agg(c.abbrv||':'||l.abbrv,',')
+from security_tags t
+left join sec_compartments c on t.compartment_fk=c.compartment_pk
+left join sec_levels l on t.level_fk=l.level_pk
+left join products p on t.product_fk=p.product_pk
+where product_fk is not NULL and c.abbrv||':'||l.abbrv = ANY(%s)"""
+        if req['vendor']=='' and req['description']=='':
+            # No filters, add the group by and query is ready to go
+            SQLstart += " group by vendor,description,product_fk having count(*)=%s"
+            cur.execute(SQLstart,(req['compartments'],len(req['compartments'])))
+        else:
+            if not req['vendor']=='' and not req['description']=='':
+                req['vendor']="%"+req['vendor']+"%"
+                req['description']="%"+req['description']+"%"
+                SQLstart += " and description ilike %s and vendor ilike %s group by vendor,description,product_fk having count(*)=%s"
+                cur.execute(SQLstart,(req['compartments'],req['description'],req['vendor'],len(req['compartments'])))
+            elif req['vendor']=='':
+                req['description']="%"+req['description']+"%"
+                SQLstart += " and description ilike %s group by vendor,description,product_fk having count(*)=%s"
+                cur.execute(SQLstart,(req['compartments'],req['description'],len(req['compartments'])))
+            elif req['description']=='':
+                req['vendor']="%"+req['vendor']+"%"
+                SQLstart += " and vendor ilike %s group by vendor,description,product_fk having count(*)=%s"
+                cur.execute(SQLstart,(req['compartments'],req['vendor'],len(req['compartments'])))
+    
+    # One of the 8 cases should've run... process the results
+    dbres = cur.fetchall()
+    listing = list()
+    for row in dbres:
+        e = dict()
+        e['vendor'] = row[0]
+        e['description'] = row[1]
+        if row[2] is None:
+            e['compartments'] = list()
+        else:
+            e['compartments'] = row[2].split(',')
+        listing.append(e)
+    
+    # Prepare the response
+    dat = dict()
+    dat['timestamp'] = req['timestamp']
+    dat['listing'] = listing
+    data = json.dumps(dat)
+
+    conn.close()
+    return data
+
+"""
 @app.route('/rest/list_products', methods=["POST"])
 def list_products():
     if request.method=='POST' and 'arguments' in request.form:
@@ -70,9 +165,9 @@ def list_products():
     conn = psycopg2.connect(dbname=dbname, host=dbhost, port=dbport)
     cur = conn.cursor()
     #no compartment
-    SQL = """
+    SQL = 
     SELECT vendor,description FROM products p
-    """
+    
     if True:  
         req['vendor']="%"+req['vendor']+"%"
         req['description']="%"+req['description']+"%"
@@ -104,7 +199,7 @@ def list_products():
     #req['vendor']
     #req['description']
     #req['compartments']
-
+"""
 
 @app.route('/rest/add_products')
 def add_products():
