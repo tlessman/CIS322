71eafe08d4750e7a13cccbce3e501e8fee5229db
Theodore <tlessman@uoregon.edu>
2017-02-17 08:33:11 +0000

    added envalid_credentials and updated login and app.py

--- src/app.py
+++ src/app.py
@@ -4,9 +4,6 @@ import psycopg2
 #import json
 #from datetime import datetime
 
-
-
-
 # GLOBALS #
 app = Flask(__name__)
 app.secret_key='c34f5286bed45063'
@@ -17,14 +14,19 @@ conn = psycopg2.connect(dbname=dbname, host=dbhost, port=dbport)
 cur = conn.cursor()
 
 # PATHS #
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', dbname=dbname, dbhost=dbhost, dbport=dbport)
-#
-
-@app.route('/login', methods=['POST'])
+@app.route('/', methods=['GET'])
+@app.route('/login', methods=['GET','POST'])
 def login():
+    if request.method =='GET':
+        return render_template('login.html')
+    if request.method =='POST' and ('username' in request.form and 'password' in request.form):
+        if not check_username(request.form['username']):
+            render_template('create_user.html')
+        if verify_password(request.form['username'], request.form['password']):
+            session['username']=request.form['username']
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template('invalid_credentials.html')
     return render_template('login.html', dbname=dbname, dbhost=dbhost, dbport=dbport)
 #
 
@@ -41,7 +43,7 @@ def create_user():
             cur.execute(SQL, data)
             conn.commit()
             session['username']=request.form['username']
-            return redirect(url_for("dashboard")) 
+            return redirect(url_for('dashboard')) 
         #
     #
     return render_template('create_user.html')
@@ -79,6 +81,14 @@ def check_username(name):
     user_res = cur.fetchall()
     return bool(user_res)
 #
+
+def verify_password(name, string):
+    SQL = "SELECT * FROM users WHERE username=%s;"
+    data = (name,)
+    cur.execute(SQL, data)
+    user_res = cur.fetchall()
+    print(user_res['password'] == string)
+    return (user_res['password'] == string)
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=8080)
