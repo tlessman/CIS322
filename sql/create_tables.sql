b3e582dd5751b99647ef85f1ab14bc373fbc6ed8
Theodore Lessman <tlessman@uoregon.edu>
2017-02-26 23:47:45 +0000

    added commentary to create_tables/sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -10,24 +10,30 @@ CREATE TABLE users (
 	user_pk serial NOT NULL,
 	username varchar(16) NOT NULL UNIQUE,
 	password varchar(16) NOT NULL,
-	role_fk integer NOT NULL REFERENCES roles.role_pk
+	role boolean NOT NULL,
+	active boolean NOT NULL
 );
 
-CREATE TABLE roles(
-	role_pk serial NOT NULL,
-	name varchar(16) NOT NULL
-);
+/*I do not think that I need roles as a separate table.  I only have two roles as of now so I am using boolean, but if others were added at a later time, changing to an int would allow for same use without overhead of a joined table. Logistics = 0; Facilities = 1; */
 
-CREATE TABLE assets(
+CREATE TABLE assets (
 	asset_pk serial NOT NULL,
 	asset_tag varchar(16) NOT NULL,
 	description varchar(64)
 );
 
-CREATE TABLE facilities)
+CREATE TABLE facilities (
 	facililty_pk serial NOT NULL,
 	common_name varchar(32) NOT NULL
 	fcode varchar(6) NOT NULL
 );
 
+/*Here, I am choosing to connect assets and facilities to an asset_status table.  I hope to later utilize this one table to reflect transit too, however, may change if that is not optimal.*/
 
+CREATE TABLE asset_status (
+	asset_fk integer REFERENCES assets.asset_pk,
+	facility_fk integer REFERENCES facilities.facility_pk,
+	arrival_dt date,
+	departure_dt date,
+	disposed boolean NOT NULL
+);
