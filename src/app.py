36d148e8c47848da5eb5506547f7647ff308a5bd
Theodore Lessman <tlessman@uoregon.edu>
2017-02-27 11:46:43 +0000

    added asset and facility addition and updated app.py and dahsshboard

--- src/app.py
+++ src/app.py
@@ -42,14 +42,7 @@ def create_user():
             password = request.form['password']
             role = request.form['role']
             cur.execute("INSERT INTO users (user_pk, username, password, role, active) VALUES (DEFAULT, '%s', '%s', '%s', TRUE);"%(username,password,role))
-#            data = (request.form['username'], request.form['password'], request.form['role'],)
-#            cur.execute(SQL, data)
-            conn.commit()
-            
-            #SQL = "INSERT INTO users (user_pk, username, password, role, active) VALUES (DEFAULT, %s, %s, %s, TRUE);"
-            #data = (request.form['username'], request.form['password'], request.form['role'],)
-            #cur.execute(SQL, data)
-            #conn.commit()
+            conn.commit() 
             session['username']=request.form['username']
             #SQL = "SELECT role FROM users WHERE usename = %s;"
             #data = {{session.username}} 
@@ -66,7 +59,7 @@ def create_user():
 def user_taken():
     if request.method == 'POST' and ('username' in request.form and 'password' in request.form):
         if check_username(request.form['username']): 
-            return redirect(url_for('create_user.html'))
+            return redirect(url_for('create_user'))
         else:
             SQL = "INSERT INTO users (user_pk, username, password, role, active,) VALUES (DEFAULT, %s, %s, %s, TRUE,);"
             data = (request.form['username'], request.form['password'], request.form['role'],)
@@ -87,9 +80,39 @@ def user_taken():
 
 @app.route('/dashboard', methods=['GET','POST'])
 def dashboard():
+    if request.method == 'GET':
+        return render_template('login.html')
     if request.method == 'POST':
         return render_template('dashboard.html')
-    return render_template('dashboard.html')
+    return render_template('login.html')
+
+@app.route('/add_facility', methods=['GET','POST'])
+def add_facility():
+    if request.method == 'GET':
+        return render_template('add_facility.html')
+    if request.method == 'POST' and ('fcode' in request.form and 'common_name' in request.form):
+        #SQL
+        fcode = request.form['fcode']
+        common_name = request.form['common_name']
+        cur.execute("INSERT INTO facilities (facility_pk, fcode, common_name) VALUES (DEFAULT, '%s', '%s');"%(fcode,common_name))
+        conn.commit()
+        return redirect(url_for('add_facility'))
+    return render_template('login.html')
+
+@app.route('/add_asset', methods=['GET', 'POST'])
+def add_asset():
+    if request.method == 'GET':
+        return render_template('add_asset.html')
+    if request.method == 'POST' and ('asset_tag' in request.form and 'description' in request.form):
+        asset_tag = request.form['asset_tag']
+        description = request.form['description']
+        #facilty = request.form['facility']
+        cur.execute("INSERT INTO assets (asset_pk, asset_tag, description) VALUES (DEFAULT, '%s', '%s');"%(asset_tag,description))
+#        cur.execute("INSERT INTO asset_status (asset_fk, facility_fk, arrival_dt, disposed) SELECT VALUES (
+        conn.commit()
+        return redirect(url_for('add_asset'))
+    return render_template('login.html')
+
 
 @app.route('/logout', methods=['POST'])
 def logout():
@@ -109,10 +132,10 @@ def verify_password(name, string):
     data = (name,)
     cur.execute(SQL, data)
     user_res = cur.fetchone()
-    #SQL = "SELECT password FROM users WHERE user_pk = %s;"
-    #data = (user_res,)
-    #cur.execute(SQL, data)
-    #user_res = cur.fetchone()
+    SQL = "SELECT password FROM users WHERE user_pk = '%s';"
+    data = (user_res,)
+    cur.execute(SQL, data)
+    user_res = cur.fetchone()
     print(user_res['password'] == string)
     return (user_res['password'] == string)
 #
